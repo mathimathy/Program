11459297894adb2b08d2e9b2933d7e009de6b2ac
mathymathi <crevettemathias2007@gmail.com>
2025-01-26 21:25:19 +0100

    objet boa

--- Python/Terminal/boa.py
+++ Python/Terminal/boa.py
@@ -3,6 +3,24 @@ import os
 import pickle
 import time
 
+class parentObj:
+	def __init__(self, inter):
+		self.var={}
+		self.func={}
+		self.inter=inter
+	
+	def addFunc(self, name, file):
+		self.func[name]=(Interpreter(self.inter.home, self.inter.stack, True, self),file)
+	
+	def get(self, var):
+		return self.var[var]
+	
+	def ret(self, v, var):
+		self.var[var]=v
+	
+	def callFunc(self, name):
+		self.func[name][0].read(self.func[name][1])
+
 class Stack:
     def __init__(self):
         self._d = []
@@ -23,7 +41,7 @@ class Stack:
 
 class Interpreter:
 
-	def __init__(self, home):
+	def __init__(self, home, stack=Stack(), objFunc=False, parentObj=None):
 		self.home=home
 		self.cmd=None
 		self.jmpPt=[]
@@ -35,7 +53,10 @@ class Interpreter:
 		self.inFct=False
 		self.cond=0
 		self.callAdress=None
-		self.stack=Stack()
+		self.object={}
+		self.stack=stack
+		self.objFunc=objFunc
+		self.parentObj=parentObj
 
 	def interprete(self, line):
 		if line=="/":
@@ -54,6 +75,11 @@ class Interpreter:
 			pass
 		elif line[:2]=="//":
 			pass
+		elif (l:=line.split(" "))[0]=="get" and self.objFunc:
+			self.var[l[1]]=self.parentObj.get(l[1])
+		elif (l:=line.split(" "))[0]=="ret" and self.objFunc:
+			v=l[1].split("->")
+			self.parentObj.ret(self.var[v[0]], v[1])
 		elif line=="}":
 			if self.inFct and self.cond==0:
 				self.cursor=self.callAdress
@@ -135,6 +161,8 @@ class Interpreter:
 				self.cmd=6
 			elif line[0]=="µ":
 				self.cmd=7
+			elif line[0]=="^":
+				self.cmd=8
 			else:
 				self.error=f"[{self.cursor+1}] The Command doesn't exist"
 		elif line[-1]=="-":
@@ -155,8 +183,10 @@ class Interpreter:
 		elif line[-2]=="-":
 			if self.cmd==0:
 				self.var[line[-1]]=line[:-2]
-			if self.cmd==5:
+			elif self.cmd==5:
 				self.var[line[-1]]=self.var[line[0]]
+			elif self.cmd==8:
+				self.var[line[-1]]=self.createObj(self.object[line[:-2]])
 		elif line[-1]=="/":
 			if self.cmd==2:
 				if self.var[line[:-1]]=="128":
@@ -171,6 +201,8 @@ class Interpreter:
 					self.var[line[-1]]=int(self.var[line[-1]])+int(line[:-2])
 				except:
 					self.var[line[-1]]=int(self.var[line[-1]])+int(self.var[line[0]])
+			elif self.cmd==8:
+				self.stack.push(self.var[line[-1]].get(line[:-2]))
 		elif line[-2]==",":
 			if self.cmd==1:
 				try:
@@ -195,6 +227,8 @@ class Interpreter:
 					self.var[line[-1]]=int(self.var[line[-1]])**int(line[:-2])
 				except:
 					self.var[line[-1]]=int(self.var[line[-1]])**int(self.var[line[0]])
+			elif self.cmd==8:
+				self.var[line[-1]].callFunc(line[:-2])
 		elif line[-2]=="\\":
 			if self.cmd==1:
 				try:
@@ -230,16 +264,36 @@ class Interpreter:
 			except Exception as e:
 				print(f"An error occured at line {self.cursor+1}: {e}")
 				quit()
+	
+	def createObj(self, data):
+		obj=parentObj(self)
+		for d in data:
+			l=d.split(":")
+			if l[1]=="get":
+				obj.var[l[0]]=self.stack.pop()
+			else:
+				obj.addFunc(l[0], l[1].replace(" ", ""))
+		return obj
 
 	def read(self, path):
 		if path[-4:]==".def":
 			with open(self.home+path) as f:
 				definitions = f.read().split("\n")
 			code=""
-			include = definitions[1].split(":")[1].split(",")
-			for el in include:
-				with open(self.home+el) as f:
-					code+=f.read()+"\n"
+			if (icl:=definitions[1].split(":")[1])!="":
+				include = icl.split(",")
+				for el in include:
+					with open(self.home+el) as f:
+						code+=f.read()+"\n"
+			if (obj:=definitions[2].split(":")[1])!="":
+				obj = obj.split(",")
+				for o in obj:
+					with open(self.home+o) as f:
+						data=f.read()
+						data=[l.strip() for l in data.split("\n")]
+						name=data[0][:-1]
+						data=data[1:-1]
+						self.object[name]=data
 			with open(self.home+definitions[0].split(":")[1]) as f:
 				code+=f.read()
 			self.code = code.split("\n")
@@ -248,6 +302,13 @@ class Interpreter:
 		elif path[-2:]==".b":
 			with open(self.home+path) as f:
 				code = f.read()
+			self.code = code.split("\n")
+			self.code = [line.strip() for line in self.code]
+			self.execute()
+		elif path[-6:]==".bFunc":
+			with open(self.home+path) as f:
+				code = f.read()
+			self.objFunc=True
 			self.code = code.split("\n")
 			self.code = [line.strip() for line in self.code]
 			self.execute()
