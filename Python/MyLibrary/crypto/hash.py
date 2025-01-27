10705908041d8f7e67c5f1bb9f7238ac468e5c0e
mathimathy <crevettemathias2007@gmail.com>
2025-01-27 10:40:56 +0100

    start of sha256

--- Python/MyLibrary/crypto/hash.py
+++ Python/MyLibrary/crypto/hash.py
@@ -8,3 +8,27 @@ def chainMod(msg, turn=20, mod=256, reverse=False):
                 lastNumber=el
             if reverse: data=newData[::-1]
         return abs(sum(data)%mod)
+
+def sha256(msg):
+	def init(m):
+		basebinary = "".join([bin(ord(char)) for char in m]).replace("0b", "")
+		words=[]
+		while len(basebinary)!=0:
+			print(basebinary)
+			if len(basebinary)<=501:
+				binary=basebinary
+				basebinary=""
+			else:
+				binary=basebinary[:501]
+				basebinary=basebinary[501:]
+			size = len(binary)
+			sizeB = bin(size)[2:]
+			binary+="1"+"".join(["O" for _ in range(512-(1+size+len(sizeB)))])+sizeB
+			words.append("0b"+binary)
+		print(words)
+	def rotr(n, amount):
+		r=n%(2**amount)
+		n=n//(2**amount)
+		n+=r*(2**(32-amount))
+		return n
+	init(msg)
