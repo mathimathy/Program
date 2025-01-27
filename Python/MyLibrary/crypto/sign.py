7a9e5d2a4ea9cd66444d7092e6769a7a56f3c854
mathymathi <crevettemathias2007@gmail.com>
2025-01-27 17:35:21 +0100

    Sha256

--- Python/MyLibrary/crypto/sign.py
+++ Python/MyLibrary/crypto/sign.py
@@ -9,11 +9,11 @@ class Sign:
             self.rsa.private = private        
 
     def sign(self, msg):
-        hashedMsg = hash.chainMod(msg)
+        hashedMsg = hash.sha256(msg)
         signed = self.rsa.cipher(hashedMsg, self.rsa.private)
         return signed
     
     def checkSign(self, msg, signed):
-        hashedMsg = hash.chainMod(msg)
+        hashedMsg = hash.sha256(msg)
         unsigned = self.rsa.decipher(signed, self.rsa.public)
         return hashedMsg==unsigned
