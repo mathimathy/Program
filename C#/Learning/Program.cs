a2b00f139f0884b767c39a412d78605812a6267f
mathimathy <crevettemathias2007@gmail.com>
2026-04-27 17:34:16 +0200

    Learning C#

---
+++ C#/Learning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            ArrayList my = new ArrayList();
+            my.Add("5");
+            Console.WriteLine(my.Count);
+        }
+    }
+}
