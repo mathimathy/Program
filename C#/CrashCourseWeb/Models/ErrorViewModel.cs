67e208117070ba25219f47c6df3c6696cf4a053b
mathimathy <crevettemathias2007@gmail.com>
2026-04-28 21:29:24 +0200

    test web

--- C#/CrashCourseWeb/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CrashCourseWeb.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
